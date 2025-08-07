fe19f0c089d7b38801c859c6b14c4c6c4838e07a
Sylphira <553312236@qq.com>
2025-08-07 23:21:46 +0800

    Add files via upload

---
+++ data/CH/Weapon/21047.js
@@ -0,0 +1,52 @@
+// Auto Generated
+
+var _weapondesc_ = {
+    "21047": "<i>「脚印。」</i><br><i>「擦痕。」</i><br><i>「气味。」</i><br><br>一切处理妥当后，他拾起一片鸦羽，轻盈跃上楼顶。<br>广袤的城市里，漆黑的影穿梭在无人注视的角落里。<br><br><i>「我将溶于影中，为你们守望。」</i>"
+}
+
+var _weaponskill_ = {
+    "21047": {
+        "v1": {
+            "Name": "隐匿",
+            "Desc": [
+                "使装备者的击破特攻提高 <color style='color:#f29e38;'>28.0%</color> / <color style='color:#f29e38;'>35.0%</color> / <color style='color:#f29e38;'>42.0%</color> / <color style='color:#f29e38;'>49.0%</color> / <color style='color:#f29e38;'>56.0%</color> 。进入战斗时或造成击破伤害后，速度提高 <color style='color:#f29e38;'>8.0%</color> / <color style='color:#f29e38;'>9.0%</color> / <color style='color:#f29e38;'>10.0%</color> / <color style='color:#f29e38;'>11.0%</color> / <color style='color:#f29e38;'>12.0%</color> ，持续 <b>2.0</b> 回合，该效果每回合只可触发1次。"
+            ]
+        },
+        "v2": {
+            "Name": "隐匿",
+            "Desc": [
+                "使装备者的击破特攻提高 <color style='color:#f29e38;'>28.0%</color> / <color style='color:#f29e38;'>35.0%</color> / <color style='color:#f29e38;'>42.0%</color> / <color style='color:#f29e38;'>49.0%</color> / <color style='color:#f29e38;'>56.0%</color> 。进入战斗时或造成击破伤害后，速度提高 <color style='color:#f29e38;'>8.0%</color> / <color style='color:#f29e38;'>9.0%</color> / <color style='color:#f29e38;'>10.0%</color> / <color style='color:#f29e38;'>11.0%</color> / <color style='color:#f29e38;'>12.0%</color> ，持续 <b>2.0</b> 回合，该效果每回合只可触发1次。"
+            ]
+        },
+        "v3": {
+            "Name": "隐匿",
+            "Desc": [
+                "使装备者的击破特攻提高 <color style='color:#f29e38;'>28.0%</color> / <color style='color:#f29e38;'>35.0%</color> / <color style='color:#f29e38;'>42.0%</color> / <color style='color:#f29e38;'>49.0%</color> / <color style='color:#f29e38;'>56.0%</color> 。进入战斗时或造成击破伤害后，速度提高 <color style='color:#f29e38;'>8.0%</color> / <color style='color:#f29e38;'>9.0%</color> / <color style='color:#f29e38;'>10.0%</color> / <color style='color:#f29e38;'>11.0%</color> / <color style='color:#f29e38;'>12.0%</color> ，持续 <b>2.0</b> 回合，该效果每回合只可触发1次。"
+            ]
+        },
+        "v4": {
+            "Name": "隐匿",
+            "Desc": [
+                "使装备者的击破特攻提高 <color style='color:#f29e38;'>28.0%</color> / <color style='color:#f29e38;'>35.0%</color> / <color style='color:#f29e38;'>42.0%</color> / <color style='color:#f29e38;'>49.0%</color> / <color style='color:#f29e38;'>56.0%</color> 。进入战斗时或造成击破伤害后，速度提高 <color style='color:#f29e38;'>8.0%</color> / <color style='color:#f29e38;'>9.0%</color> / <color style='color:#f29e38;'>10.0%</color> / <color style='color:#f29e38;'>11.0%</color> / <color style='color:#f29e38;'>12.0%</color> ，持续 <b>2.0</b> 回合，该效果每回合只可触发1次。"
+            ]
+        },
+        "v5": {
+            "Name": "隐匿",
+            "Desc": [
+                "使装备者的击破特攻提高 <color style='color:#f29e38;'>28.0%</color> / <color style='color:#f29e38;'>35.0%</color> / <color style='color:#f29e38;'>42.0%</color> / <color style='color:#f29e38;'>49.0%</color> / <color style='color:#f29e38;'>56.0%</color> 。进入战斗时或造成击破伤害后，速度提高 <color style='color:#f29e38;'>8.0%</color> / <color style='color:#f29e38;'>9.0%</color> / <color style='color:#f29e38;'>10.0%</color> / <color style='color:#f29e38;'>11.0%</color> / <color style='color:#f29e38;'>12.0%</color> ，持续 <b>2.0</b> 回合，该效果每回合只可触发1次。"
+            ]
+        },
+        "v6": {
+            "Name": "隐匿",
+            "Desc": [
+                "使装备者的击破特攻提高 <color style='color:#f29e38;'>28.0%</color> / <color style='color:#f29e38;'>35.0%</color> / <color style='color:#f29e38;'>42.0%</color> / <color style='color:#f29e38;'>49.0%</color> / <color style='color:#f29e38;'>56.0%</color> 。进入战斗时或造成击破伤害后，速度提高 <color style='color:#f29e38;'>8.0%</color> / <color style='color:#f29e38;'>9.0%</color> / <color style='color:#f29e38;'>10.0%</color> / <color style='color:#f29e38;'>11.0%</color> / <color style='color:#f29e38;'>12.0%</color> ，持续 <b>2.0</b> 回合，该效果每回合只可触发1次。"
+            ]
+        },
+        "Live": {
+            "Name": "隐匿",
+            "Desc": [
+                "使装备者的击破特攻提高 <color style='color:#f29e38;'>28%</color> / <color style='color:#f29e38;'>35%</color> / <color style='color:#f29e38;'>42%</color> / <color style='color:#f29e38;'>49%</color> / <color style='color:#f29e38;'>56%</color> 。进入战斗时或造成击破伤害后，速度提高 <color style='color:#f29e38;'>8%</color> / <color style='color:#f29e38;'>9%</color> / <color style='color:#f29e38;'>10%</color> / <color style='color:#f29e38;'>11%</color> / <color style='color:#f29e38;'>12%</color> ，持续 <b>2</b> 回合，该效果每回合只可触发1次。"
+            ]
+        }
+    }
+}
