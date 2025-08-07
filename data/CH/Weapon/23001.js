5bf3ff57034dccd9561b34fb2d813257ce1edcbf
Sylphira <553312236@qq.com>
2025-08-07 23:23:52 +0800

    Add files via upload

---
+++ data/CH/Weapon/23001.js
@@ -0,0 +1,16 @@
+// Auto Generated
+
+var _weapondesc_ = {
+    "23001": "少女露出一丝不易察觉的微笑。<br><i>「为什么呢？」<br>「此时此刻此地，明明只有我一个人……」<br>「却感觉…很热闹呢。」</i>"
+}
+
+var _weaponskill_ = {
+    "23001": {
+        "Live": {
+            "Name": "花与蝶",
+            "Desc": [
+                "使装备者的暴击率提高 <color style='color:#f29e38;'>18%</color> / <color style='color:#f29e38;'>21%</color> / <color style='color:#f29e38;'>24%</color> / <color style='color:#f29e38;'>27%</color> / <color style='color:#f29e38;'>30%</color> 。当装备者在战斗中速度大于 <b>100</b> 时，每超过 <b>10</b> 点，普攻和战技造成的伤害提高 <color style='color:#f29e38;'>6%</color> / <color style='color:#f29e38;'>7%</color> / <color style='color:#f29e38;'>8%</color> / <color style='color:#f29e38;'>9%</color> / <color style='color:#f29e38;'>10%</color> ，同时终结技的暴击伤害提高 <color style='color:#f29e38;'>12%</color> / <color style='color:#f29e38;'>14%</color> / <color style='color:#f29e38;'>16%</color> / <color style='color:#f29e38;'>18%</color> / <color style='color:#f29e38;'>20%</color> ，该效果可叠加 <b>6</b> 层。"
+            ]
+        }
+    }
+}
---
+++ data/CH/Weapon/23031.js
@@ -0,0 +1,52 @@
+// Auto Generated
+
+var _weapondesc_ = {
+    "23031": "<i>「可悲可笑的奴隶们……」</i><br>浩荡的丰饶民军队压境而来，狼毒如洪水般涌向青丘军，试图唤起军士深埋于心中的恐惧，化作种种可怖的幻象，使他们垂下武器，犹豫不前。<br>但阵线犹如铜墙铁壁，不为步离人轻蔑的话音所动，一个身影缓步走出。<br>她缓缓活动着身体，无形的煞气冲击下，旗帜猎猎作响。<br><i>「将军，是将军出现了……」</i><br><i>「飞霄将军……」</i><br><i>「『天击』将军！」</i><br>细微的呼唤汇聚起来，化作震天的战吼。<br><br><i>「翾翔不坠，万载常胜！」</i><br>她站在那里，一个人，就像一支军队。"
+}
+
+var _weaponskill_ = {
+    "23031": {
+        "v1": {
+            "Name": "震慑",
+            "Desc": [
+                "使装备者的暴击率提高 <color style='color:#f29e38;'>15.0%</color> / <color style='color:#f29e38;'>17.5%</color> / <color style='color:#f29e38;'>20.0%</color> / <color style='color:#f29e38;'>22.5%</color> / <color style='color:#f29e38;'>25.0%</color> 。我方任意单体施放攻击后，装备者获得1层【流光】，最多叠加 <b>6.0</b> 层。每层【流光】使装备者造成的终结技伤害无视目标 <color style='color:#f29e38;'>9.0%</color> / <color style='color:#f29e38;'>10.0%</color> / <color style='color:#f29e38;'>11.0%</color> / <color style='color:#f29e38;'>12.0%</color> / <color style='color:#f29e38;'>13.0%</color> 的防御力，该效果在装备者施放终结技后解除。"
+            ]
+        },
+        "v2": {
+            "Name": "震慑",
+            "Desc": [
+                "使装备者的暴击率提高 <color style='color:#f29e38;'>15.0%</color> / <color style='color:#f29e38;'>17.5%</color> / <color style='color:#f29e38;'>20.0%</color> / <color style='color:#f29e38;'>22.5%</color> / <color style='color:#f29e38;'>25.0%</color> 。我方任意单体目标施放攻击后，装备者获得1层【流光】，最多叠加 <b>6.0</b> 层。每层【流光】使装备者造成的终结技伤害无视目标 <color style='color:#f29e38;'>9.0%</color> / <color style='color:#f29e38;'>10.0%</color> / <color style='color:#f29e38;'>11.0%</color> / <color style='color:#f29e38;'>12.0%</color> / <color style='color:#f29e38;'>13.0%</color> 的防御力，该效果在装备者施放终结技后解除。"
+            ]
+        },
+        "v3": {
+            "Name": "震慑",
+            "Desc": [
+                "使装备者的暴击率提高 <color style='color:#f29e38;'>15.0%</color> / <color style='color:#f29e38;'>17.5%</color> / <color style='color:#f29e38;'>20.0%</color> / <color style='color:#f29e38;'>22.5%</color> / <color style='color:#f29e38;'>25.0%</color> 。装备者施放追加攻击时，获得1层【流光】，最多叠加 <b>2.0</b> 层。每层【流光】使装备者造成的终结技伤害无视目标 <color style='color:#f29e38;'>27.0%</color> / <color style='color:#f29e38;'>30.0%</color> / <color style='color:#f29e38;'>33.0%</color> / <color style='color:#f29e38;'>36.0%</color> / <color style='color:#f29e38;'>39.0%</color> 的防御力。装备者回合结束时，移除1层【流光】。"
+            ]
+        },
+        "v4": {
+            "Name": "震慑",
+            "Desc": [
+                "使装备者的暴击率提高 <color style='color:#f29e38;'>15.0%</color> / <color style='color:#f29e38;'>17.5%</color> / <color style='color:#f29e38;'>20.0%</color> / <color style='color:#f29e38;'>22.5%</color> / <color style='color:#f29e38;'>25.0%</color> 。装备者施放追加攻击时，获得1层【流光】，最多叠加 <b>2.0</b> 层。每层【流光】使装备者造成的终结技伤害无视目标 <color style='color:#f29e38;'>27.0%</color> / <color style='color:#f29e38;'>30.0%</color> / <color style='color:#f29e38;'>33.0%</color> / <color style='color:#f29e38;'>36.0%</color> / <color style='color:#f29e38;'>39.0%</color> 的防御力。装备者回合结束时，移除1层【流光】。"
+            ]
+        },
+        "v5": {
+            "Name": "震慑",
+            "Desc": [
+                "使装备者的暴击率提高 <color style='color:#f29e38;'>15.0%</color> / <color style='color:#f29e38;'>17.5%</color> / <color style='color:#f29e38;'>20.0%</color> / <color style='color:#f29e38;'>22.5%</color> / <color style='color:#f29e38;'>25.0%</color> 。装备者施放追加攻击时，获得1层【流光】，最多叠加 <b>2.0</b> 层。每层【流光】使装备者造成的终结技伤害无视目标 <color style='color:#f29e38;'>27.0%</color> / <color style='color:#f29e38;'>30.0%</color> / <color style='color:#f29e38;'>33.0%</color> / <color style='color:#f29e38;'>36.0%</color> / <color style='color:#f29e38;'>39.0%</color> 的防御力。装备者回合结束时，移除1层【流光】。"
+            ]
+        },
+        "v6": {
+            "Name": "震慑",
+            "Desc": [
+                "使装备者的暴击率提高 <color style='color:#f29e38;'>15.0%</color> / <color style='color:#f29e38;'>17.5%</color> / <color style='color:#f29e38;'>20.0%</color> / <color style='color:#f29e38;'>22.5%</color> / <color style='color:#f29e38;'>25.0%</color> 。装备者施放追加攻击时，获得1层【流光】，最多叠加 <b>2.0</b> 层。每层【流光】使装备者造成的终结技伤害无视目标 <color style='color:#f29e38;'>27.0%</color> / <color style='color:#f29e38;'>30.0%</color> / <color style='color:#f29e38;'>33.0%</color> / <color style='color:#f29e38;'>36.0%</color> / <color style='color:#f29e38;'>39.0%</color> 的防御力。装备者回合结束时，移除1层【流光】。"
+            ]
+        },
+        "Live": {
+            "Name": "震慑",
+            "Desc": [
+                "使装备者的暴击率提高 <color style='color:#f29e38;'>15%</color> / <color style='color:#f29e38;'>17.5%</color> / <color style='color:#f29e38;'>20%</color> / <color style='color:#f29e38;'>22.5%</color> / <color style='color:#f29e38;'>25%</color> 。装备者施放追加攻击时，获得1层【流光】，最多叠加 <b>2</b> 层。每层【流光】使装备者造成的终结技伤害无视目标 <color style='color:#f29e38;'>27%</color> / <color style='color:#f29e38;'>30%</color> / <color style='color:#f29e38;'>33%</color> / <color style='color:#f29e38;'>36%</color> / <color style='color:#f29e38;'>39%</color> 的防御力。装备者回合结束时，移除1层【流光】。"
+            ]
+        }
+    }
+}
